14681b90bd19e90d450fbf476681922bf94930fb
DaniloCHGS <danilochagasbm@gmail.com>
2022-04-12 15:36:29 -0300

    Menu com links dinamicos

--- app/Controller/Admin/Page.php
+++ app/Controller/Admin/Page.php
@@ -7,6 +7,26 @@ use App\Utils\View;
 class Page
 {
     /**
+     * Módulos do painel
+     */
+    private static $modules = [
+        'home' => [
+            'label' => 'Home',
+            'link' => URL . '/admin',
+            'icon' => 'bx-home-circle'
+        ],
+        'testimonies' => [
+            'label' => 'Depoimentos',
+            'link' => URL . '/depoimentos',
+            'icon' => 'bx-message-rounded-dots'
+        ],
+        'users' => [
+            'label' => 'Usuários',
+            'link' => URL . '/usuarios',
+            'icon' => 'bx-user'
+        ],
+    ];
+    /**
      * Retorna o conteudo (view) da estrutura principal do painel
      */
     public static function getPage($title, $content)
@@ -19,9 +39,24 @@ class Page
     /**
      * Renderiza o menu
      */
-    private static function getMenu($currentModule){
+    private static function getMenu($currentModule)
+    {
+        //Links do menu
+        $links = "";
+
+        foreach (self::$modules as $hash => $module) {
+            $links .= View::render('admin/menu/link', [
+                'label' => $module['label'],
+                'link'  => $module['link'],
+                'icon'  => $module['icon'],
+                'current'  => $hash == $currentModule ? 'active' : ''
+            ]);
+        }
+
         //Retorna renderização do menu
-        return View::render("admin/menu/box", []);
+        return View::render("admin/menu/box", [
+            'links' => $links
+        ]);
     }
     /**
      * Retorna a estrutura do painel com conteudo dinamico
@@ -30,10 +65,10 @@ class Page
     {
         //Renderiza a view do painel
         $contentPanel = View::render('admin/panel', [
-            'menu'=> self::getMenu($currentModule),
+            'menu' => self::getMenu($currentModule),
             'content' => $content
         ]);
         //Retorna a página renderizada
-        return self::getPage($title,$contentPanel);
+        return self::getPage($title, $contentPanel);
     }
 }
