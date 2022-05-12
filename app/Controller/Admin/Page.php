5b74481dc790111e5532a5b819989ae871f3d130
DaniloCHGS <danilochagasbm@gmail.com>
2022-05-12 17:58:04 -0300

    Autenticação pelo menu

--- app/Controller/Admin/Page.php
+++ app/Controller/Admin/Page.php
@@ -4,7 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Utils\View;
 use App\Utils\UpFile;
-
+use App\Model\Entity\AccessArea as EntityAccessArea;
+use App\Utils\Utils;
 
 class Page
 {
@@ -16,44 +17,32 @@ class Page
         'home' => [
             'label' => 'Home',
             'link' => URL . '/admin',
-            'icon' => 'bx-home-circle',
-            'access_area' => 1,
-            'uri' => 'admin'
+            'icon' => 'bx-home-circle'
         ],
         'testimonies' => [
             'label' => 'Depoimentos',
             'link' => URL . '/admin/depoimentos',
-            'icon' => 'bx-message-rounded-dots',
-            'access_area' => 1,
-            'uri' => 'depoimentos'
+            'icon' => 'bx-message-rounded-dots'
         ],
         'company' => [
             'label' => 'Dados da Empresa',
             'link' => URL . '/admin/dados-empresa',
-            'icon' => 'bx-buildings',
-            'access_area' => 1,
-            'uri' => 'dados_empresa'
+            'icon' => 'bx-buildings'
         ],
         'identity' => [
             'label' => 'Identidade do Site',
             'link' => URL . '/admin/identidade-site',
-            'icon' => 'bx-palette',
-            'access_area' => 1,
-            'uri' => 'identidade_site'
+            'icon' => 'bx-palette'
         ],
         'access_area' => [
             'label' => 'Área de Acesso',
             'link' => URL . '/admin/area-de-acesso',
-            'icon' => 'bx-cog',
-            'access_area' => 3,
-            'uri' => 'area_de_acesso'
+            'icon' => 'bx-cog'
         ],
         'users' => [
             'label' => 'Usuários',
             'link' => URL . '/admin/usuarios',
-            'icon' => 'bx-user',
-            'access_area' => 2,
-            'uri' => 'usuarios'
+            'icon' => 'bx-user'
         ],
     ];
     /**
@@ -71,6 +60,9 @@ class Page
      */
     private static function getMenu($currentModule)
     {
+        $entityModules = self::getModules();
+
+        Utils::pre($entityModules);
         //Links do menu
         $links = "";
 
@@ -87,6 +79,20 @@ class Page
         return View::render("admin/menu/box", [
             'links' => $links
         ]);
+    }
+    /**
+     * Busca todos módulos cadastrados no banco
+     */
+    private static function getModules()
+    {
+        $entityModules = [];
+
+        $results = EntityAccessArea::getAccess();
+
+        while ($entityModule = $results->fetchObject(EntityAccessArea::class)) {
+            $entityModules[] = $entityModule;
+        }
+        return $entityModules;
     }
     /**
      * Retorna a estrutura do painel com conteudo dinamico
@@ -143,8 +149,9 @@ class Page
             "links" => $links
         ]);
     }
-    public static function uploadFile($file, $path, $config = []){
-        if($file['error'] != 4 and !empty($path)){
+    public static function uploadFile($file, $path, $config = [])
+    {
+        if ($file['error'] != 4 and !empty($path)) {
             $fileUploaded = new UpFile($file, $path, $config);
             return $fileUploaded;
         }
