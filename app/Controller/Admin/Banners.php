ebd800726904ebf1aff11654c3acbb45566a0409
DaniloCHGS <danilochagasbm@gmail.com>
2022-05-27 14:42:04 -0300

    Banner finalizado

--- app/Controller/Admin/Banners.php
+++ app/Controller/Admin/Banners.php
@@ -4,7 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Utils\View;
 use App\Utils\Utils;
-use App\Model\Entity\Depoimentos as DepoimentosModel;
+use App\Model\Entity\Banner as EntityBanner;
 use \WilliamCosta\DatabaseManager\Pagination;
 
 class Banners extends Page
@@ -20,13 +20,13 @@ class Banners extends Page
 
         switch ($queryParams['status']) {
             case 'created':
-                return Alert::getSuccess('Depoimento criado com sucesso');
+                return Alert::getSuccess('Banner criado com sucesso');
                 break;
             case 'updated':
-                return Alert::getSuccess('Depoimento atualizado com sucesso');
+                return Alert::getSuccess('Banner atualizado com sucesso');
                 break;
             case 'deleted':
-                return Alert::getSuccess('Depoimento excluido com sucesso');
+                return Alert::getSuccess('Banner excluido com sucesso');
                 break;
         }
     }
@@ -38,140 +38,164 @@ class Banners extends Page
     {
         //Conteudo de Depoimentos
         $content = View::render('admin/modules/banners/index', [
-            'itens' => self::getTestimoniesItens($request, $pagination),
+            'itens' => self::getBannerItens($request, $pagination),
             'pagination' => parent::getPagination($request, $pagination),
             'status' => self::getStatus($request)
         ]);
 
         //Retorna página completa
-        return parent::getPanel('Boss | Depoimentos', $content, 'testimonies');
+        return parent::getPanel('Boss | Banners', $content, 'banners');
     }
 
     /**
      * Retorna formulário de cadastro
      */
-    public static function getNewTestimonies($request)
+    public static function getNewBanner($request)
     {
 
         //Conteudo do formulário
-        $content = View::render('admin/modules/testimonies/form', [
-            'title' => 'Cadastro de Depoimento',
+        $content = View::render('admin/modules/banners/form', [
+            'title' => 'Cadastro de Banner',
             'autor' => '',
             'depoimento' => '',
             'status' => ''
         ]);
 
         //Retorna página completa
-        return parent::getPanel('Boss | Depoimentos', $content, 'testimonies');
+        return parent::getPanel('Boss | Banners', $content, 'banners');
     }
 
     /**
      * Cadastra depoimento
      */
-    public static function setNewTestimonies($request)
+    public static function setNewBanner($request)
     {
         //Dados
-        $postVars   = $request->getPostVars();
+        $postVars = $request->getPostVars();
+        $fileVars = $request->getFileVars();
+        
+        $banner = new EntityBanner;
+        $banner->title = $postVars['title'];
+        $banner->link = $postVars['link'];
+        $banner->link_target = !isset($postVars['link_target']) ? 0 : 1;
+        $banner->active = !isset($postVars['active']) ? 0 : 1;
 
-        $depoimento = new DepoimentosModel;
-        $depoimento->autor = $postVars['autor'];
-        $depoimento->depoimento = $postVars['depoimento'];
-        $depoimento->register();
+        $banner_desktop = parent::uploadFile($fileVars['banner_desktop'], 'banner/');
+        $banner_mobile  = parent::uploadFile($fileVars['banner_mobile'],  'banner/mobile/');
+        
+        $banner->banner_desktop = $banner_desktop->new_name;
+        $banner->banner_mobile = $banner_mobile->new_name;
+        $banner->register();
 
-        $request->getRouter()->redirect('/admin/depoimentos/' . $depoimento->id . '/edit?status=created');
+        $request->getRouter()->redirect('/admin/banners/' . $banner->id . '/edit?status=created');
     }
 
     /**
      * Form para editar depoimento
      */
-    public static function getEditTestimonies($request, $id)
+    public static function getEditBanner($request, $id)
     {
-        $depoimento = DepoimentosModel::getTestimonyById($id);
+        $banner = EntityBanner::getBannerById($id);
 
-        if (!$depoimento instanceof DepoimentosModel) {
-            $request->getRouter()->redirect('/admin/depoimentos');
+        if (!$banner instanceof EntityBanner) {
+            $request->getRouter()->redirect('/admin/banners');
         }
 
         //Conteudo do formulário
-        $content = View::render('admin/modules/testimonies/form', [
+        $content = View::render('admin/modules/banners/form', [
             'title' => 'Editar Depoimento',
-            'autor' => $depoimento->autor,
-            'depoimento' => $depoimento->depoimento,
-            'status' => self::getStatus($request)
+            'status' => self::getStatus($request),
+            'title' => $banner->title,
+            'link' => $banner->link,
+            'banner_desktop' => $banner->banner_desktop,
+            'banner_mobile' => $banner->banner_mobile,
+            'active' => $banner->active,
+            'link_target' => $banner->link_target
         ]);
 
         //Retorna página completa
-        return parent::getPanel('Boss | Depoimentos', $content, 'testimonies');
+        return parent::getPanel('Boss | Banners', $content, 'banners');
     }
 
     /**
      * Editar depoimento
      */
-    public static function setEditTestimonies($request, $id)
+    public static function setEditBanner($request, $id)
     {
-        $depoimento = DepoimentosModel::getTestimonyById($id);
+        $banner = EntityBanner::getBannerById($id);
 
-        if (!$depoimento instanceof DepoimentosModel) {
-            $request->getRouter()->redirect('/admin/depoimentos');
+        if (!$banner instanceof EntityBanner) {
+            $request->getRouter()->redirect('/admin/banners');
         }
 
-        $postVars   = $request->getPostVars();
+        $postVars = $request->getPostVars();
+        $fileVars = $request->getFileVars();
 
-        $depoimento->autor = $postVars['autor'] ?? $depoimento->autor;
-        $depoimento->depoimento = $postVars['depoimento'] ?? $depoimento->depoimento;
+        $banner->title = $postVars['title'] ?? $banner->title;
+        $banner->link = $postVars['link'] ?? $banner->link;
+        $banner->link_target = !isset($postVars['link_target']) ? 0 : 1;
+        $banner->active = !isset($postVars['active']) ? 0 : 1;
 
-        $depoimento->update();
+        if($fileVars['banner_desktop']['error'] != 4){
+            $banner_desktop = parent::uploadFile($fileVars['banner_desktop'], 'banner/');
+            $banner->banner_desktop = $banner_desktop->new_name;
+        }
 
-        $request->getRouter()->redirect('/admin/depoimentos/' . $depoimento->id . '/edit?status=updated');
+        if($fileVars['banner_mobile']['error'] != 4){
+            $banner_mobile  = parent::uploadFile($fileVars['banner_mobile'],  'banner/mobile/');
+            $banner->banner_mobile = $banner_mobile->new_name;
+        }
+        $banner->update();
+
+        $request->getRouter()->redirect('/admin/banners/' . $banner->id . '/edit?status=updated');
     }
 
     /**
      * Tela de deletar um depoimento 
      */
-    public static function getDeleteTestimonies($request, $id)
+    public static function getDeleteBanner($request, $id)
     {
-        $depoimento = DepoimentosModel::getTestimonyById($id);
+        $banner = EntityBanner::getBannerById($id);
 
-        if (!$depoimento instanceof DepoimentosModel) {
-            $request->getRouter()->redirect('/admin/depoimentos');
+        if (!$banner instanceof EntityBanner) {
+            $request->getRouter()->redirect('/admin/banners');
         }
 
         //Conteudo do formulário
-        $content = View::render('admin/modules/testimonies/delete', [
-            'title' => 'Excluir Depoimento',
-            'autor' => $depoimento->autor,
-            'depoimento' => $depoimento->depoimento,
+        $content = View::render('admin/modules/banners/delete', [
+            'title' => $banner->title,
+            'banner_desktop' => $banner->banner_desktop
         ]);
 
         //Retorna página completa
-        return parent::getPanel('Boss | Depoimentos', $content, 'testimonies');
+        return parent::getPanel('Boss | Banners', $content, 'banners');
     }
 
     /**
      * Deletar depoimento
      */
-    public static function setDeleteTestimonies($request, $id)
+    public static function setDeleteBanner($request, $id)
     {
-        $depoimento = DepoimentosModel::getTestimonyById($id);
+        $banner = EntityBanner::getBannerById($id);
 
-        if (!$depoimento instanceof DepoimentosModel) {
-            $request->getRouter()->redirect('/admin/depoimentos');
+        if (!$banner instanceof EntityBanner) {
+            $request->getRouter()->redirect('/admin/banners');
         }
 
-        $depoimento->delete();
+        $banner->delete();
 
-        $request->getRouter()->redirect('/admin/depoimentos?status=deleted');
+        $request->getRouter()->redirect('/admin/banners?status=deleted');
     }
 
     /**
      * Renderiza os itens de depoimento na página
      */
-    private function getTestimoniesItens($request, &$pagination)
+    private function getBannerItens($request, &$pagination)
     {
         $itens = "";
 
         //Quantidade total de registros
-        $total = DepoimentosModel::getTestimonies(null, null, null, "COUNT(*) as qtd")->fetchObject()->qtd;
+        $total = EntityBanner::getBanners(null, null, null, "COUNT(*) as qtd")->fetchObject()->qtd;
 
         //Pagina atual
         $queryParams = $request->getQueryParams();
@@ -181,17 +205,17 @@ class Banners extends Page
         $pagination = new Pagination($total, $paginaAtual, 3);
 
         //Resultados da página
-        $results = DepoimentosModel::getTestimonies(null, 'id DESC', $pagination->getLimit());
+        $results = EntityBanner::getBanners(null, 'id DESC', $pagination->getLimit());
 
         //Renderiza o item
-        while ($obDepoimentos = $results->fetchObject(DepoimentosModel::class)) {
+        while ($obBanner = $results->fetchObject(EntityBanner::class)) {
             $itens .= View::render(
-                "admin/modules/testimonies/itens",
+                "admin/modules/banners/itens",
                 [
-                    "autor" => $obDepoimentos->autor,
-                    "depoimento" => $obDepoimentos->depoimento,
-                    "data" => date("d/m/Y H:i:s", strtotime($obDepoimentos->data)),
-                    "id" => $obDepoimentos->id,
+                    'banner_desktop' => $obBanner->banner_desktop,
+                    'title' => $obBanner->title,
+                    'active' => $obBanner->active == 1 ? 'Ativo' : 'Desativado',
+                    'id' => $obBanner->id
                 ]
             );
         }
--- routes/admin/banners.php
+++ routes/admin/banners.php
@@ -16,73 +16,73 @@ $router->get('/admin/banners', [
 ]);
 
 //Tela de cadastro
-$router->get('/admin/depoimentos/new', [
+$router->get('/admin/banner/new', [
     'middlewares' => [
         'required-admin-login',
         'time-login',
         'module-auth'
     ],
     function ($request) {
-        return new Response(200, Admin\Testimony::getNewTestimonies($request));
+        return new Response(200, Admin\Banners::getNewBanner($request));
     }
 ]);
 
 //Cadastro
-$router->post('/admin/depoimentos/new', [
+$router->post('/admin/banner/new', [
     'middlewares' => [
         'required-admin-login',
         'time-login',
         'module-auth'
     ],
     function ($request) {
-        return new Response(200, Admin\Testimony::setNewTestimonies($request));
+        return new Response(200, Admin\Banners::setNewBanner($request));
     }
 ]);
 
 //Tela de editar
-$router->get('/admin/depoimentos/{id}/edit', [
+$router->get('/admin/banners/{id}/edit', [
     'middlewares' => [
         'required-admin-login',
         'time-login',
         'module-auth'
     ],
     function ($request, $id) {
-        return new Response(200, Admin\Testimony::getEditTestimonies($request, $id));
+        return new Response(200, Admin\Banners::getEditBanner($request, $id));
     }
 ]);
 
 //Editar
-$router->post('/admin/depoimentos/{id}/edit', [
+$router->post('/admin/banners/{id}/edit', [
     'middlewares' => [
         'required-admin-login',
         'time-login',
         'module-auth'
     ],
     function ($request, $id) {
-        return new Response(200, Admin\Testimony::setEditTestimonies($request, $id));
+        return new Response(200, Admin\Banners::setEditBanner($request, $id));
     }
 ]);
 
 //Tela de excluir
-$router->get('/admin/depoimentos/{id}/delete', [
+$router->get('/admin/banners/{id}/delete', [
     'middlewares' => [
         'required-admin-login',
         'time-login',
         'module-auth'
     ],
     function ($request, $id) {
-        return new Response(200, Admin\Testimony::getDeleteTestimonies($request, $id));
+        return new Response(200, Admin\Banners::getDeleteBanner($request, $id));
     }
 ]);
 
 //Excluir
-$router->post('/admin/depoimentos/{id}/delete', [
+$router->post('/admin/banners/{id}/delete', [
     'middlewares' => [
         'required-admin-login',
         'time-login',
         'module-auth'
     ],
     function ($request, $id) {
-        return new Response(200, Admin\Testimony::setDeleteTestimonies($request, $id));
+        return new Response(200, Admin\Banners::setDeleteBanner($request, $id));
     }
 ]);
